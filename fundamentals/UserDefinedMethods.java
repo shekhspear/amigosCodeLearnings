e86c936f5ce8e12351844e5e2fb8ae3d542ada12
shekhspear <shekhar.karande@gmail.com>
2024-02-11 00:51:44 +0000

    push changes

--- fundamentals/UserDefinedMethods.java
+++ fundamentals/UserDefinedMethods.java
@@ -1,13 +1,16 @@
+import org.jetbrains.annotations.Contract;
+
 public class UserDefinedMethods {
     public static void main(String[] args) {
         char [] characters = {'A','A','B','C','D','D','D'};
-        System.out.println(countOfCharacters(characters,'Z'));
+        System.out.println(countOfCharacters(characters,'D'));
 
     }
+    @Contract(pure = true)
     public static int countOfCharacters (char [] letters, char searchLetter){
         int count = 0;
         for (char letter : letters) {
-            if (letter == searchLetter) count ++;
+            if (letter == searchLetter) count++;
         }
         return count;
     }
